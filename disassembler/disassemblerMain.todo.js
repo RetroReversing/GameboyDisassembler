9cd707deddc59ecffbda30d5d6a7bc893f627114
Alasdair Morrison <amorri40@gmail.com>
2017-09-04 08:41:10 +0100

    Added experimental todos to keep me on track

---
+++ disassembler/disassemblerMain.todo.js
@@ -0,0 +1,20 @@
+import * as assert from 'assert';
+import { describe, it } from 'mocha';
+
+describe('Goals for the project:', function () {
+  it('should be able to open zip files as roms', function () {
+    assert.fail('not implemented');
+  });
+  it('should be able to return the number of instructions it found in a rom', function () {
+    assert.fail('not implemented');
+  });
+  it('should be able to run through a folder of roms and return an array of instruction numbers per rom', function () {
+    assert.fail('not implemented');
+  });
+  it('should be able to create a Code/Data Logger file .cdl', function () {
+    assert.fail('not implemented');
+  });
+  it('should be able to disassembler into a js like format', function () {
+    assert.fail('not implemented');
+  });
+});
